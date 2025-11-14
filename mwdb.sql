8da0f58cc98c305b43a775b6e1cebbf8e8ff07e4
kareen67 <karomi.edu@gmail.com>
2025-11-14 05:58:39 -0300

    bs

--- mwdb.sql
+++ mwdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-11-2025 a las 18:34:49
+-- Tiempo de generación: 14-11-2025 a las 09:55:39
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
